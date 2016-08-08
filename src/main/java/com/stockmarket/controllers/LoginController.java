d006a401177e21483321f03eca6f8c33b88e489c
YashdeepSharma <sharma.yashdeep@outlook.com>
2016-08-08 14:38:31 +0530

    Created

--- src/main/java/com/stockmarket/controllers/LoginController.java
+++ src/main/java/com/stockmarket/controllers/LoginController.java
@@ -1,6 +1,5 @@
 package com.stockmarket.controllers;
 
-import com.stockmarket.models.CompanyStock;
 import com.stockmarket.models.Person;
 import com.stockmarket.services.CompanyStockService;
 import com.stockmarket.services.PersonService;
@@ -8,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
